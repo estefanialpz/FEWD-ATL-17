c1db4050c38401b4ce4dc5abcd5baa9e8ee41e3e
Nia <estefanialpz.0@gmail.com>
2017-12-26 14:49:13 -0500

    EL hw week 4
    intro to jQuery - final

--- Week 4/homework/EL homework/js/index.js
+++ Week 4/homework/EL homework/js/index.js
@@ -7,6 +7,10 @@ $(document).ready(function() {
 	$('.readless').click(showReadMore);
 	$('.learnmore').click(showLearnMoreText);
 	$('.learnmore').click(hideLearnMore);
+	$('.learnmore').click(showLearnLess);
+	$('.learnless').click(hideLearnLess);
+	$('.learnless').click(hideLearnMoreText);
+	$('.learnless').click(showLearnMore)
 
 	function showText(event) {
 		event.preventDefault();
@@ -46,5 +50,24 @@ $(document).ready(function() {
 	function showReadMore(event) {
 		event.preventDefault();
 		$('.readmore').show();
+	}
+
+	function showLearnLess(event) {
+		event.preventDefault();
+		$('.learnless').show();
+	}
+
+	function hideLearnLess(event) {
+		event.preventDefault();
+		$('.learnless').hide();
+	}
+
+	function hideLearnMoreText(event) {
+		event.preventDefault();
+		$('#learnmoretext').slideUp();
+	}
+
+	function showLearnMore(event) {
+		$('.learnmore').show();
 	}
 });
