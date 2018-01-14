1c8d283651ac721fcf1696a8ca5564dc008c26fc
Nia <estefanialpz.0@gmail.com>
2018-01-14 17:29:51 -0500

    homeedit

--- Week 5/lesson 9/starter_code/blackout/js/index.js
+++ Week 5/lesson 9/starter_code/blackout/js/index.js
@@ -1,17 +1,18 @@
 $(document).ready(function() {
 
-	var lights = 'on';
 
-	$("#light_switch").click(switchLights);
+var lights = 'on';
 
-	function switchLights () {
-		if (lights == 'on') {
-			$('body').addClass('dark');
-			lights = 'off';
-		} else {
-			$('body').removeClass('dark');
-			lights = 'on';
-		}
-	}	
+$('#light_switch').click(switchLights);
+
+function switchLights () {
+	if (lights == 'on') {
+		$('body').addClass('dark');
+		lights = 'off';
+	} else {
+		$('body').removeClass('dark');
+		lights = 'on';
+	}
+};
 
 });
