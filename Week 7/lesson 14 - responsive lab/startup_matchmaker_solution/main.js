da4bbceed16e6a747d36b07ae079ad5480faef79
Nia <estefanialpz.0@gmail.com>
2018-02-07 21:20:45 -0500

    postclass

---
+++ Week 7/lesson 14 - responsive lab/startup_matchmaker_solution/main.js
@@ -0,0 +1,14 @@
+$('#show-nav').click(function() {
+
+
+	$('#main-nav').slideToggle();
+
+	/* if($(this).hasClass('closed')){
+		$('#main-nav').slideDown();
+		$(this).removeClass('closed');
+	}else{
+		$('#main-nav').slideUp();
+		$(this).addClass('closed');
+	} */
+
+})
